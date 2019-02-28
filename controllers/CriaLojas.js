5847cee79807a50ae3a0128e2f699173cdaeb7b4
Paulo da Luz <paulo.daluz@compasso.com.br>
2019-02-28 13:35:52 -0300

    incluida validação pelo express

--- controllers/CriaLojas.js
+++ controllers/CriaLojas.js
@@ -1,15 +1,30 @@
 //pagamentos.js
 
 module.exports = function (app) {
-    app.get('/criaLojas', function (req, res) {
-        console.log('Recebida requisicao de teste na porta 3000.')
-        res.send('OK.');
-    });
-
-
     app.post("/criaLojas/criaLoja", function (req, res) {
         var loja = req.body;
         console.log('processando aguarde...');
+
+        var loja = req.body;
+
+        req.assert("nome_loja", "Nome da loja é obrigatório.").notEmpty();
+        req.assert("endereco", "Endereço da loja é obrigatório.").notEmpty();
+        req.assert("celular", "Telefone/Celular da loja é obrigatório e deve ser um decimal.").notEmpty().len(10,12);
+        req.assert("cnpj", "CNPJ da loja é obrigatório.").notEmpty().len(18);
+        req.assert("horarioDeTrabalho", "Horário de trabalho é obrigatório").notEmpty();
+        req.assert("cidade", "Cidade é obrigatória.").notEmpty();
+        req.assert("estado", "Estado é obrigatório e deve ter 2 caracteres").notEmpty().len(2, 2);
+
+        var errors = req.validationErrors();
+
+        if (errors) {
+            console.log("Erros de validação encontrados");
+            res.status(400).send(errors);
+            return;
+        }
+        console.log('criando loja...');
+
+
 
         var connection = app.persistencia.connectionFactory();
         var lojaDao = new app.persistencia.LojaDao(connection); 
